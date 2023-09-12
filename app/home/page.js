be469a826ac8e74faba37b86fd9e0d2c85223e40
cris <cristian.villanueva@passare.com>
2023-09-12 11:18:53 -0500

    package, tailwind extended with font

--- app/home/page.js
+++ app/home/page.js
@@ -1,33 +1,50 @@
 "use client";
 import { motion } from "framer-motion";
 import PageFadeIn from "@/Components/PageFadeIn";
-import { Luckiest_Guy, Pacifico } from "next/font/google";
-
-const Pacificoo = Pacifico({ subsets: ["latin"], weight: "400" });
-const luckiestGuy = Luckiest_Guy({ subsets: ["latin"], weight: "400" });
+import Link from "next/link";
 
 export default function Home() {
   return (
     <PageFadeIn>
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 text-center">
         <div>
-          <motion.h1
-            className={`${luckiestGuy.className} text-9xl text-[#1DB954]`}
-            initial={{ scale: 0.5, opacity: 0.5 }}
-            animate={{ scale: 1, opacity: 1 }}
-            transition={{ type: "spring", stiffness: 120 }}
-          >
+          <h1 className={`font-josefinBold text-9xl text-[#1DB954]`}>
             MUSIC +
-          </motion.h1>
-          <motion.h1
-            className={`${Pacificoo.className} text-9xl`}
-            initial={{ scale: 0.5, opacity: 0.5 }}
-            animate={{ scale: 1, opacity: 1 }}
-            transition={{ type: "spring", stiffness: 120 }}
+          </h1>
+          <h1 className={`font-josefinBold text-9xl`}>LOVE?</h1>
+          <motion.h3
+            className={`font-josefin`}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 1,
+              type: "spring",
+              stiffness: 100,
+              duration: 0.8,
+            }}
           >
-            LOVE?
-          </motion.h1>
+            Use your music taste to calculate your compatibility 👀
+          </motion.h3>
         </div>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          whileHover={{ scale: 1.1 }}
+          transition={{
+            opacity: {
+              delay: 1,
+              type: "spring",
+              stiffness: 100,
+              duration: 0.8,
+            },
+            y: { delay: 1, type: "spring", stiffness: 100, duration: 0.8 },
+          }}
+          className=" rounded bg-white text-black p-3 m-4"
+        >
+          <Link className={"font-josefin"} href={"/spotifyLink"}>
+            Try it out
+          </Link>
+        </motion.div>
       </div>
     </PageFadeIn>
   );
---
+++ app/spotifyLink/page.js
@@ -0,0 +1,12 @@
+"use client";
+import PageFadeIn from "@/Components/PageFadeIn";
+
+export default function spotifyLink(second) {
+  return (
+    <PageFadeIn>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 text-center">
+        <h1 className="font-heading text-9xl">hi</h1>
+      </div>
+    </PageFadeIn>
+  );
+}
